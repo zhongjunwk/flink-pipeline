ab0979e3838da505a3b91670637b8d81eb99b734
zhongjun <zhongjun@idss-cn.com>
2025-02-27 09:45:06 +0800

    init

--- pipeline-job/pipeline-job-processor-submitter/src/main/java/com/zj/flink/job/sumbmitter/FlinkPipelineJobSubmitter.java
+++ pipeline-job/pipeline-job-processor-submitter/src/main/java/com/zj/flink/job/sumbmitter/FlinkPipelineJobSubmitter.java
@@ -1,26 +1,28 @@
-package com.zj.flink.mock.job.sumbmitter;
+package com.zj.flink.job.sumbmitter;
 
-import com.zj.flink.context.FlinkAnnotationConfigApplicationContext;
-import com.zj.flink.context.FlinkApplicationConfigContext;
-import com.zj.flink.config.util.FlinkConfigUtil;
 import com.zj.flink.config.FlinkPipelineConfiguration;
 import com.zj.flink.config.bean.FlinkPipelineConfig;
+import com.zj.flink.config.util.FlinkConfigUtil;
+import com.zj.flink.context.FlinkAnnotationConfigApplicationContext;
+import com.zj.flink.context.FlinkApplicationConfigContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Map;
 
 /**
- * Flink Kafka滑动窗口示例数据生产作业
+ * Flink Kafka滑动窗口示例作业
  */
 @Slf4j
-public class KafkaSlidingWindowExampleDataProducerJobSubmitter {
+public class FlinkPipelineJobSubmitter {
+
     /**
      * 主函数
      *
      * @param args 命令行参数
+     * @throws Exception 异常
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 使用注解配置类加载 Spring 应用上下文
         AnnotationConfigApplicationContext applicationContext = new FlinkAnnotationConfigApplicationContext(null, FlinkApplicationConfigContext.class).getApplicationContext();
         Map<String, FlinkPipelineConfiguration> configurationMap = applicationContext.getBeansOfType(FlinkPipelineConfiguration.class);
